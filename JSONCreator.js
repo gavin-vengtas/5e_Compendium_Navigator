8e582c894ceabbd41639a35d8c6243dfe4a350a1
GVengtas <vengtasgc@gmail.com>
2023-10-15 20:57:34 +0200

    Fix Creature Names

--- JSONCreator.js
+++ JSONCreator.js
@@ -15,18 +15,34 @@ data.compendium.monster.forEach((monster) => {
             //read each available trait
             monster.trait.forEach((element) => {
                 let description = "";
+                let fullNamesArr = monster.name.split(" ");
+                let totalNames = fullNamesArr.length-1;
                 traitJSON.compendium.trait[element.name] = traitJSON.compendium.trait[element.name]?traitJSON.compendium.trait[element.name]:[];
                 //combine lines and append to traitJSON
                 element.text.forEach((sentence)=>{
-                    description += sentence + "\n";
+                    if (sentence) {
+                        description += sentence + "\n";                        
+                    }
                 });
 
-                traitJSON.compendium.trait[element.name].push(description.replace(new RegExp(monster.name,"g"),"creature"));
+                if(totalNames>0){
+                    for(let index = 0; index < totalNames; index++){
+                        description = description.replace(new RegExp(fullNamesArr[index]+" ","g"),"");
+                        description = description.replace(new RegExp(fullNamesArr[index].toLocaleLowerCase()+" ","g"),"");
+                    }
+                }
+
+                description = description.replace(new RegExp(fullNamesArr[totalNames],"g"),"creature");
+                description = description.replace(new RegExp(fullNamesArr[totalNames].toLocaleLowerCase(),"g"),"creature");
+
+                console.log(description);
+
+                traitJSON.compendium.trait[element.name].push(description);
             });
         }
-});
+});     
 
-fs.writeFile('traits.json', JSON.stringify(traitJSON), 'utf8', ()=>{
+fs.writeFile('traits2.json', JSON.stringify(traitJSON), 'utf8', ()=>{
     console.log('Written to file: traits.json');
 });
 
